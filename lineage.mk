19d5b5a6c81021e22552a4b2fcda1395518cd7e8
BrightICE17 <38675204+erikml1@users.noreply.github.com>
2019-04-13 19:05:54 -0700

    lineage 15.1

--- lineage.mk
+++ lineage.mk
@@ -5,10 +5,10 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Enhanced NFC
-# $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# $(call inherit-product, vendor/lineage/config/nfc_enhanced.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="zerofltetmo" \
