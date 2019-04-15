9a514d0d4cbb82bdf800e1f4207b3879cdb45cc0
BrightICE17 <38675204+erikml1@users.noreply.github.com>
2019-04-15 13:52:38 -0700

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -16,6 +16,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_NAME := lineage_zerofltetmo
 PRODUCT_DEVICE := zerofltetmo
-
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=zerofltetmo TARGET_DEVICE=zerofltetmo BUILD_FINGERPRINT="samsung/zerofltetmo/zerofltetmo:7.0/NRD90M/G920TUVS6FRC1:user/release-keys" PRIVATE_BUILD_DESC="zerofltetmo-user 7.0 NRD90M G920TUVS6FRC1 release-keys"
