3205f01001f22aa6210e6b97427287d128a1c1b6
BrightICE17 <38675204+erikml1@users.noreply.github.com>
2019-05-02 16:53:19 -0700

    zero: added build description

--- lineage.mk
+++ lineage.mk
@@ -13,6 +13,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="zerofltetmo" \
     TARGET_DEVICE="zeroflte"
+    BUILD_DESC="zerofltetmo-user 7.0 NRD90M G920TUVS6FRC1 release-keys"
 
 PRODUCT_NAME := lineage_zerofltetmo
 PRODUCT_DEVICE := zerofltetmo
